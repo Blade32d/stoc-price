b97696141469ff1f66c4d8f40b57d8a8dd3b421a
Blade32c <118992260+Blade32d@users.noreply.github.com>
2023-08-27 22:10:28 +0530

    Basic code

---
+++ stock-price.py
@@ -0,0 +1,90 @@
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+import seaborn as sns
+import os
+from datetime import datetime
+
+import warnings
+warnings.filterwarnings("ignore")
+
+gstock_data = pd.read_csv('GOOG.csv')
+
+gstock_data = gstock_data [['Date','Close']]
+
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.plot(gstock_data.Date,gstock_data.Close,color='red')
+plt.show()
+
+close_data = gstock_data.filter(['Close'])
+dataset = close_data.values
+training = int(np.ceil(len(dataset) * .85))
+print(training)
+
+from sklearn.preprocessing import MinMaxScaler
+
+scaler = MinMaxScaler(feature_range=(0, 1))
+scaled_data = scaler.fit_transform(dataset)
+
+train_data = scaled_data[0:int(training), :]
+# prepare feature and labels
+x_train = []
+y_train = []
+
+for i in range(60, len(train_data)):
+    x_train.append(train_data[i-60:i, 0])
+    y_train.append(train_data[i, 0])
+
+x_train, y_train = np.array(x_train), np.array(y_train)
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+
+model = keras.models.Sequential()
+model.add(keras.layers.LSTM(units=64,
+                            return_sequences=True,
+                            input_shape=(x_train.shape[1], 1)))
+model.add(keras.layers.LSTM(units=64))
+model.add(keras.layers.Dense(32))
+model.add(keras.layers.Dropout(0.5))
+model.add(keras.layers.Dense(1))
+model.summary
+
+model.compile(optimizer='adam',
+              loss='mean_squared_error')
+history = model.fit(x_train,
+                    y_train,
+                    epochs=100)
+
+test_data = scaled_data[training - 60:, :]
+x_test = []
+y_test = dataset[training:, :]
+for i in range(60, len(test_data)):
+    x_test.append(test_data[i-60:i, 0])
+
+x_test = np.array(x_test)
+x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+
+# predict the testing data
+predictions = model.predict(x_test)
+predictions = scaler.inverse_transform(predictions)
+
+# evaluation metrics
+mse = np.mean(((predictions - y_test) ** 2))
+print("MSE", mse)
+print("RMSE", np.sqrt(mse))
+
+train = gstock_data[:training]
+test = gstock_data[training:]
+test['Predictions'] = predictions
+
+plt.figure(figsize=(10, 8))
+plt.plot(train['Close'])
+plt.plot(test[['Close', 'Predictions']])
+plt.title('Stock Close Price')
+plt.xlabel('Date')
+plt.ylabel("Close")
+plt.legend(['Train', 'Test', 'Predictions'])
+
